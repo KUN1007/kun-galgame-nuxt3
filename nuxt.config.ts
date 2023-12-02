6d077bb99af6c374b14192d59e068a5cf17f30ef
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-12-02 18:37:41 +0800

    chore: dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true, override: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   modules: [
-    "@pinia/nuxt",
-    "@nuxt/image",
-    "@pinia-plugin-persistedstate/nuxt",
+    '@pinia/nuxt',
+    '@nuxt/image',
+    '@nuxtjs/i18n',
+    '@pinia-plugin-persistedstate/nuxt',
+    'dayjs-nuxt',
+    '@nuxtjs/robots',
+    '@formkit/auto-animate',
+    'nuxt-icon',
+    '@vite-pwa/nuxt',
+    'nuxt-typed-router',
   ],
 })
