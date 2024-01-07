309b1612037c9796584092c4583d366ecb914b32
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-01-07 21:37:33 +0800

    feat: technique

--- pages/technique/index.vue
+++ pages/technique/index.vue
@@ -1,5 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { showKUNGalgamePageWidth } = storeToRefs(useKUNGalgameSettingsStore())
 
-<template></template>
+const techniquePageWidth = computed(() => {
+  return showKUNGalgamePageWidth.value.technique + '%'
+})
+</script>
 
-<style lang="scss" scoped></style>
+<template>
+  <div class="root">
+    <div class="content">
+      <!-- TODO: -->
+      <h1 style="margin: auto">
+        {{ $t('technique.KKKKK') }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.root {
+  height: calc(100vh - 65px);
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  padding: 5px;
+  height: 100%;
+  transition: width 0.2s;
+  width: v-bind(techniquePageWidth);
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--kungalgame-trans-white-5);
+  color: var(--kungalgame-font-color-3);
+  border: 1px solid var(--kungalgame-trans-blue-2);
+  border-radius: 5px;
+  overflow-y: scroll;
+}
+</style>
