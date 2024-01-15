a87bbfb6f51b2666edee946dd2f24b36edfe7a37
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-01-15 20:43:44 +0800

    feat: message frame

--- components/kun/top-bar/MessageBox.vue
+++ components/kun/top-bar/MessageBox.vue
@@ -5,10 +5,36 @@ const { showKUNGalgameMessageBox } = storeToRefs(useTempSettingStore())
 <template>
   <Transition
     enter-active-class="animate__animated animate__fadeInRight animate__faster"
-    appear
+    leave-active-class="animate__animated animate__fadeOutRight animate__faster"
   >
-    <div class="root">
-      <div>box</div>
+    <div class="root" v-if="showKUNGalgameMessageBox">
+      <div class="title">
+        <span class="name">Message</span>
+        <span class="icon-item" @click="showKUNGalgameMessageBox = false">
+          <Icon name="line-md:close" />
+        </span>
+      </div>
+
+      <div class="container">
+        <div class="message">
+          <div class="top">
+            <span class="status">
+              <Icon name="line-md:alert-circle" />
+              <Icon name="line-md:confirm-circle" />
+            </span>
+            <span class="time">11111111111111</span>
+          </div>
+
+          <div class="content">
+            Your post was ${actionType} by ${actionUserId}
+          </div>
+
+          <div class="bottom">
+            <span>Read</span>
+            <span>Delete</span>
+          </div>
+        </div>
+      </div>
     </div>
   </Transition>
 </template>
@@ -24,5 +50,45 @@ const { showKUNGalgameMessageBox } = storeToRefs(useTempSettingStore())
   border-left: 1px solid var(--kungalgame-blue-1);
   border-radius: 5px 0 0 5px;
   background-color: var(--kungalgame-trans-white-2);
+  color: var(--kungalgame-font-color-3);
+}
+
+.title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  font-size: 20px;
+
+  .icon-item {
+    cursor: pointer;
+  }
+}
+
+.container {
+  padding: 10px;
+}
+
+.message {
+  padding: 10px;
+  border: 1px solid var(--kungalgame-blue-1);
+  border-radius: 5px;
+  background-color: var(--kungalgame-trans-blue-0);
+
+  .top {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .content {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    word-break: break-all;
+  }
+
+  .bottom {
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
