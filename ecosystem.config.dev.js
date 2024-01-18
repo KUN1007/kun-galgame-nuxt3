d97900ead66cfe2a04a91a84edea0b3686255104
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-01-18 23:39:19 +0800

    pref: pm2 config

---
+++ ecosystem.config.dev.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  apps: [
+    {
+      name: 'kun-visual-novel',
+      port: 7777,
+      cwd: path.join(__dirname),
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
+      script: './.output/server/index.mjs',
+    },
+  ],
+}
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,10 @@
-const path = require('path')
-
 module.exports = {
   apps: [
     {
       name: 'kun-visual-novel',
       port: 7777,
-      cwd: path.join(__dirname),
-      instances: 3,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '2G',
+      exec_mode: 'cluster',
+      instances: 'max',
       script: './.output/server/index.mjs',
     },
   ],
