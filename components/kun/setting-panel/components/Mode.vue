6229b1a6d84415c33472fca6f6a0f58589ed1530
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-02-16 18:34:10 +0800

    feat: mode active style

--- components/kun/setting-panel/components/Mode.vue
+++ components/kun/setting-panel/components/Mode.vue
@@ -1,30 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Mode {
+  name: string
+  icon: string
+}
+
+const modeItem: Mode[] = [
+  {
+    name: 'light',
+    icon: 'line-md:moon-filled-alt-to-sunny-filled-loop-transition',
+  },
+  {
+    name: 'dark',
+    icon: 'line-md:sunny-outline-to-moon-loop-transition',
+  },
+  {
+    name: 'system',
+    icon: 'line-md:light-dark-loop',
+  },
+]
+</script>
 
 <template>
   <div class="mode">
     <span>{{ $t('header.settings.mode') }}</span>
     <div class="mode-container">
-      <span>
+      <span
+        v-for="(mode, index) in modeItem"
+        :key="index"
+        :class="$colorMode.preference === mode.name ? 'active' : ''"
+      >
         <Icon
           class="sun"
-          name="line-md:moon-filled-alt-to-sunny-filled-loop-transition"
-          @click="$colorMode.preference = 'light'"
-        />
-      </span>
-
-      <span>
-        <Icon
-          class="moon"
-          name="line-md:sunny-outline-to-moon-loop-transition"
-          @click="$colorMode.preference = 'dark'"
-        />
-      </span>
-
-      <span>
-        <Icon
-          class="moon"
-          name="line-md:light-dark-loop"
-          @click="$colorMode.preference = 'system'"
+          :name="mode.icon"
+          @click="$colorMode.preference = mode.name"
         />
       </span>
     </div>
@@ -44,17 +52,29 @@
   width: 60%;
   display: flex;
   justify-content: space-around;
+  align-items: center;
+
   span {
+    display: flex;
     cursor: pointer;
+    align-items: center;
+    padding-bottom: 2px;
   }
+
   span:nth-child(1) {
     color: var(--kungalgame-red-4);
   }
+
   span:nth-child(2) {
     color: var(--kungalgame-blue-5);
   }
+
   span:nth-child(3) {
     color: var(--kungalgame-purple-4);
   }
+}
+
+.active {
+  border-bottom: 2px solid var(--kungalgame-blue-5);
 }
 </style>
