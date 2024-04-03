728e2747c49ad1ff68a4f6d448c437a82f5d15d1
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-04-03 17:46:35 +0800

    fix: #40

--- utils/markdownToText.ts
+++ utils/markdownToText.ts
@@ -3,8 +3,8 @@ export const markdownToText = (markdown: string) => {
     .replace(/(\*\*|__)(.*?)\1/gs, '$2')
     .replace(/(\*|_)(.*?)\1/gs, '$2')
     .replace(/#+\s*(.*?)\n/g, '$1\n')
+    .replace(/\[!\[.*?\]\(.*?\)\]\(.*?\)/g, '')
     .replace(/!\[(.*?)\]\((.*?)\)/gm, '$1')
-    .replace(/`([^`]+)`/g, '$1')
-    .replace(/```[\s\S]+?```/gs, '')
+    .replace(/(```\w+)([\s\S]*?)(```)/g, '$2')
     .replace(/[<>~\\]/g, '')
 }
