35341c85fe9f3407e6e71c77bb5520335ebafe1e
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-09-22 15:45:57 +0800

    feat: add acgs, remove nekotaku

--- components/friend-links/friends.ts
+++ components/friend-links/friends.ts
@@ -96,10 +96,9 @@ export const galgame: Friend[] = [
     label: '梓澪のGalgame仓库'
   },
   {
-    name: 'nekotaku',
-    link: 'https://www.nekotaku.xyz/',
-    label:
-      '喵源领域（原神域同萌）是一家致力于让二次元爱好者更便捷地获取信息和找到自己所需内容的综合二次元内容站点，欢迎您的访问！'
+    name: 'acgs',
+    link: 'https://acgs.one/',
+    label: '专注分享次元世界 - Galgame, 游戏, 免费, 下载'
   }
 ]
 
