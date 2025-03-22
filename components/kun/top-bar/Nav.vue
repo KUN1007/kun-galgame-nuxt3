6de2695f4b1078f04d527c80d917fa95c68e340b
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-03-23 03:21:42 +0800

    feat: add a topbar back button

--- components/kun/top-bar/Nav.vue
+++ components/kun/top-bar/Nav.vue
@@ -5,6 +5,13 @@ const { showKUNGalgameHamburger, messageStatus } = storeToRefs(
   useTempSettingStore()
 )
 
+const router = useRouter()
+const canGoBack = ref(false)
+
+const updateCanGoBack = () => {
+  canGoBack.value = window.history.length > 2
+}
+
 watch(
   () => route.name,
   () => {
@@ -21,16 +28,50 @@ onMounted(async () => {
   } else {
     messageStatus.value = 'new'
   }
+
+  updateCanGoBack()
+
+  router.afterEach(() => {
+    updateCanGoBack()
+  })
 })
 </script>
 
 <template>
   <div class="cursor-pointer">
-    <Icon
-      class="icon"
-      name="lucide:menu"
+    <KunButton
+      :is-icon-only="true"
+      color="default"
+      size="xl"
+      variant="light"
       @click="showKUNGalgameHamburger = true"
-    />
+      class-name="flex sm:hidden"
+    >
+      <Icon name="lucide:menu" />
+    </KunButton>
+
+    <div class="hidden sm:block">
+      <KunButton
+        v-if="canGoBack"
+        :is-icon-only="true"
+        color="default"
+        size="xl"
+        variant="light"
+        @click="router.back()"
+      >
+        <Icon name="lucide:arrow-left" />
+      </KunButton>
+      <KunButton
+        v-else
+        :is-icon-only="true"
+        color="default"
+        size="xl"
+        variant="light"
+        @click="() => navigateTo('/')"
+      >
+        <Icon name="lucide:home" />
+      </KunButton>
+    </div>
 
     <LazyKunTopBarHamburger />
   </div>
