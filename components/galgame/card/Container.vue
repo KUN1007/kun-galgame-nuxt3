3a852ce2c1ddb576e8b6ffe72a35384a992e0675
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-05-02 20:43:26 +0800

    feat: add galgame page nsfw alert

--- components/galgame/card/Container.vue
+++ components/galgame/card/Container.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 const pageData = storeToRefs(useTempGalgameStore())
+const { showKUNGalgameContentLimit } = storeToRefs(usePersistSettingsStore())
 
 const { data, status } = await useFetch(`/api/galgame`, {
   method: 'GET',
   query: pageData,
   ...kungalgameResponseHandler
 })
+
+const isNSFWEnable = computed(
+  () =>
+    showKUNGalgameContentLimit.value === 'nsfw' ||
+    showKUNGalgameContentLimit.value === 'all'
+)
+const description = isNSFWEnable.value
+  ? '网站已启用 NSFW, 杂鱼~♡ 臭杂鱼♡, 请注意您周围没有人'
+  : '网站未启用 NSFW, 部分 Galgame 不可见, 要查看所有 Galgame, 请在网站右上角打开 NSFW'
 </script>
 
 <template>
@@ -20,6 +30,22 @@ const { data, status } = await useFetch(`/api/galgame`, {
           <KunSettingPanelComponentsNSFW />
         </template>
       </KunHeader>
+    </KunCard>
+
+    <KunCard
+      :is-hoverable="false"
+      :is-transparent="false"
+      :color="isNSFWEnable ? 'danger' : 'warning'"
+    >
+      <div class="flex items-center gap-2">
+        <KunIcon
+          :class="
+            cn('h-5 w-5', isNSFWEnable ? 'text-danger' : 'text-warning-600')
+          "
+          :name="isNSFWEnable ? 'lucide:ban' : 'lucide:shield-check'"
+        />
+        <p>{{ description }}</p>
+      </div>
     </KunCard>
 
     <GalgameCardNav />
