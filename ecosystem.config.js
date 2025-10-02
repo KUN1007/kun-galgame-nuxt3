b275257e75d1360ee692a882ce8495cf57a1521c
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-10-03 04:34:39 +0800

    fix: ReferenceError: module is not defined

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-module.exports = {
+export default {
   apps: [
     {
       name: 'kun-visual-novel',
