176a4e7a50e19fa2cb5554c3d23e7652a2242ab8
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-10-03 05:11:48 +0800

    fix: prisma/schema.prisma: file not found

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from 'prisma/config'
 import 'dotenv/config'
 
 export default defineConfig({
-  schema: './prisma/schema',
-  migrations: {
-    path: './prisma/migrations'
-  }
+  schema: './prisma/schema'
 })
