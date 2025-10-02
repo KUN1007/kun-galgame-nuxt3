466cf19d9765748ca56da8aabad22751ac0c945d
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-10-03 04:33:54 +0800

    fix: ReferenceError: require is not defined

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'url'
-const path = require('path')
+import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
